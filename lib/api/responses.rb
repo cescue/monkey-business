a64e924d842bef2397e161a72e7192fd6c7e809b
chris escue <chris@escue.io>
2016-12-02 15:14:13 -0800

    Add default options hash

--- lib/api/responses.rb
+++ lib/api/responses.rb
@@ -5,7 +5,7 @@ require_relative 'api_resource.rb'
 module MonkeyBusiness
   # Abstraction of the Surveymonkey responses resource  and associated methods
   class Responses < ApiResource
-    def bulk(options)
+    def bulk(options = {})
       @options.merge!(options)
       @path += '/bulk'
       self
