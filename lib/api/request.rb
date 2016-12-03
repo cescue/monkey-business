0fd02d43146dd28bad8627948b4a030bfacf6200
chris escue <chris@escue.io>
2016-12-02 16:28:09 -0800

    Minor refactoring

--- lib/api/request.rb
+++ lib/api/request.rb
@@ -9,47 +9,53 @@ module MonkeyBusiness
   # Abstraction of an HTTP request sent to the surveymonkey API
   class HttpRequest
     def self.request(access_token, uri, options = {})
+      @access_token = access_token
       @uri = URI(uri)
 
       @http_method = options[:method] ? options.delete(:method).to_sym : :get
       @options = options
 
-      raise HttpMethodError.new(@http_method) unless respond_to?(@http_method)
-
-      request = send(@http_method)
+      raise HttpMethodError, @http_method unless respond_to?(@http_method)
 
       @http = Net::HTTP.new(@uri.host, @uri.port)
       @http.use_ssl = true
 
-      request['Authorization'] = "bearer #{access_token}"
-      request['Content-Type'] = 'application/json'
-
-      response = @http.request(request)
-
-      if [:get, :post].include?(@http_method)
-        JSON.parse(response.body)
-      elsif @http_method == :options
-        response['Allow'].split(',').map { |x| x.downcase.to_sym  }
-      elsif @http_method == :head
-        response.to_hash
-      end
+      send(@http_method)
     end
 
     def self.get
       params = @options.map { |k, v| "#{k}=#{v}" }.join('&')
-      Net::HTTP::Get.new("#{@uri.request_uri}?#{params}")
+
+      request = Net::HTTP::Get.new("#{@uri.request_uri}?#{params}")
+      request['Authorization'] = "bearer #{@access_token}"
+      request['Content-Type'] = 'application/json'
+
+      response = @http.request(request)
+
+      JSON.parse(response.body)
     end
 
-    def self.post(options = {})
-      raise NotImplementedError.new
+    def self.post
+      raise NotImplementedError, 'Not Yet Implemented'
     end
 
-    def self.head(options = {})
-      Net::HTTP::Head.new(@uri.request_uri)
+    def self.head
+      request = Net::HTTP::Head.new(@uri.request_uri)
+      request['Authorization'] = "bearer #{@access_token}"
+      request['Content-Type'] = 'application/json'
+
+      @http.request(request).to_hash
     end
 
-    def self.options(options = {})
-      Net::HTTP::Options.new(@uri.request_uri)
+    def self.options
+      request = Net::HTTP::Options.new(@uri.request_uri)
+
+      request['Authorization'] = "bearer #{@access_token}"
+      request['Content-Type'] = 'application/json'
+
+      response = @http.request(request)
+
+      response['Allow'].split(',').map { |x| x.downcase.to_sym }
     end
   end
 end
