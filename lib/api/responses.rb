8e5703ecdcf8d4f2a59db693c7f65f1ac6df9e8e
chris escue <chris@escue.io>
2016-12-02 16:02:19 -0800

    Implement GET .../responses/:id/details

--- lib/api/responses.rb
+++ lib/api/responses.rb
@@ -5,6 +5,12 @@ require_relative 'api_resource.rb'
 module MonkeyBusiness
   # Abstraction of the Surveymonkey responses resource  and associated methods
   class Responses < ApiResource
+    def details(options = {})
+      @options.merge!(options)
+      @path += '/details'
+      self
+    end
+
     def bulk(options = {})
       @options.merge!(options)
       @path += '/bulk'
